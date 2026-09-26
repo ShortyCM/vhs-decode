ef63df073c0b6d69716a9a9a255f88135565b0bc
ShortyCM <39855412+ShortyCM@users.noreply.github.com>
2026-09-25 21:22:46 -0700

    Use shared-memory process workers for existing VHS RF blocks

--- vhsdecode/demodcache.py
+++ vhsdecode/demodcache.py
@@ -1,32 +1,287 @@
+import multiprocessing
+import threading
 import time
+import types
+from multiprocessing import shared_memory
+
+import numpy as np
+
 from lddecode.core import DemodCache
 from vhsdecode.addons.gnuradioZMQ import ZMQSend, ZMQReceive
 
 
+_WORKER_RF_ATTRIBUTES = (
+    "DecoderParams",
+    "Filters",
+    "_chroma_trap",
+    "_disable_diff_demod",
+    "_do_cafc",
+    "_high_boost",
+    "_notch",
+    "_options",
+    "_sub_emphasis_params",
+    "_use_fsc_notch_filter",
+    "_video_eq",
+    "blockcut",
+    "blockcut_end",
+    "blocklen",
+    "chromaTrap",
+    "freq_hz",
+    "system",
+    "PAL_V4300D_NotchFilter",
+    "freq",
+)
+
+
+def _plain_value(value):
+    if hasattr(value, "_asdict"):
+        return types.SimpleNamespace(**value._asdict())
+    return value
+
+
+def _make_worker_rf_state(rf):
+    state = {}
+    for attribute in _WORKER_RF_ATTRIBUTES:
+        if hasattr(rf, attribute):
+            state[attribute] = _plain_value(getattr(rf, attribute))
+    state["debug_plot"] = None
+    return state
+
+
+def _vhs_demod_process(input_queue, output_queue, rf_class, rf_state):
+    rf = rf_class.__new__(rf_class)
+    rf.__dict__.update(rf_state)
+
+    while True:
+        item = input_queue.get()
+
+        if item is None or item[0] == "END":
+            return
+
+        if item[0] == "DEMOD_SHM":
+            _, token, blocknum, shm_name, shape, dtype_str, request = item
+            shm = shared_memory.SharedMemory(name=shm_name)
+            try:
+                data = np.ndarray(shape, dtype=np.dtype(dtype_str), buffer=shm.buf)
+                output = rf.demodblock(
+                    data=data,
+                    fftdata=None,
+                    mtf_level=0,
+                    cut=True,
+                )
+                output["request"] = request
+                output["MTF"] = 0
+                output_queue.put((blocknum, output, token))
+            finally:
+                shm.close()
+
+        elif item[0] == "SYNC_SHM":
+            _, token, blocknum, shm_name, shape, dtype_str = item
+            shm = shared_memory.SharedMemory(name=shm_name)
+            try:
+                data = np.ndarray(shape, dtype=np.dtype(dtype_str), buffer=shm.buf)
+                output = {
+                    "sync": rf.demodblock_sync(
+                        data=data,
+                        fftdata=None,
+                        cut=True,
+                    )
+                }
+                output_queue.put((blocknum, output, token))
+            finally:
+                shm.close()
+
+
+class _VHSJobDispatcher:
+    def __init__(self, worker_queues):
+        self.worker_queues = worker_queues
+        self.next_worker = 0
+        self.next_token = 0
+        self.shared_inputs = {}
+
+    def _select_queue(self):
+        queue = self.worker_queues[self.next_worker]
+        self.next_worker = (self.next_worker + 1) % len(self.worker_queues)
+        return queue
+
+    def _share_array(self, array):
+        array = np.ascontiguousarray(array)
+        shm = shared_memory.SharedMemory(create=True, size=array.nbytes)
+        target = np.ndarray(array.shape, dtype=array.dtype, buffer=shm.buf)
+        target[...] = array
+
+        token = self.next_token
+        self.next_token += 1
+        self.shared_inputs[token] = shm
+        return token, shm.name, array.shape, array.dtype.str
+
+    def put(self, item):
+        if item is None:
+            self._select_queue().put(None)
+            return
+
+        if isinstance(item, tuple) and len(item) == 4 and not isinstance(item[0], str):
+            blocknum, block, mtf, request = item
+            item = ("DEMOD", blocknum, block, mtf, request)
+
+        kind = item[0]
+
+        if kind == "DEMOD":
+            blocknum, block, _, request = item[1:]
+            token, name, shape, dtype_str = self._share_array(block["rawinput"])
+            self._select_queue().put(
+                ("DEMOD_SHM", token, blocknum, name, shape, dtype_str, request)
+            )
+            return
+
+        if kind == "SYNC":
+            blocknum, block = item[1:]
+            token, name, shape, dtype_str = self._share_array(block["rawinput"])
+            self._select_queue().put(
+                ("SYNC_SHM", token, blocknum, name, shape, dtype_str)
+            )
+            return
+
+        if kind == "NEWPARAMS":
+            for queue in self.worker_queues:
+                queue.put(item)
+            return
+
+        self._select_queue().put(item)
+
+    def qsize(self):
+        total = 0
+        for queue in self.worker_queues:
+            try:
+                total += queue.qsize()
+            except (NotImplementedError, AttributeError):
+                pass
+        return total
+
+    def release(self, token):
+        shm = self.shared_inputs.pop(token, None)
+        if shm is None:
+            return
+        try:
+            shm.close()
+        finally:
+            try:
+                shm.unlink()
+            except FileNotFoundError:
+                pass
+
+    def close(self):
+        for token in list(self.shared_inputs):
+            self.release(token)
+
+
+class _VHSResultQueue:
+    def __init__(self, queue, dispatcher):
+        self.queue = queue
+        self.dispatcher = dispatcher
+
+    def get(self):
+        result = self.queue.get()
+        if result is None:
+            return None
+
+        if isinstance(result, tuple) and len(result) == 3:
+            blocknum, output, token = result
+            self.dispatcher.release(token)
+            return blocknum, output
+
+        return result
+
+    def put(self, item):
+        self.queue.put(item)
+
+
 class DemodCacheTape(DemodCache):
     def __init__(self, *args, **kwargs):
-        super(DemodCacheTape, self).__init__(*args, **kwargs)
-        self._gnrc_afe = args[0].options.gnrc_afe
-        if self._gnrc_afe:
-            self.zmqsend = ZMQSend()
-            self.zmqreceive = ZMQReceive()
-            print(
-                "Open GNURadio with ZMQ REQ source set at tcp://localhost:%d and ZMQ REP sink set at tcp://*:%d"
-                % (self.zmqsend.port, self.zmqreceive.port)
+        rf = args[0]
+        self._gnrc_afe = rf.options.gnrc_afe
+
+        base_args = list(args)
+        if "num_worker_threads" in kwargs:
+            requested_workers = kwargs["num_worker_threads"]
+        elif len(base_args) >= 6:
+            requested_workers = base_args[5]
+        else:
+            requested_workers = 6
+
+        if requested_workers <= 0 or self._gnrc_afe:
+            super(DemodCacheTape, self).__init__(*args, **kwargs)
+            if self._gnrc_afe:
+                self.zmqsend = ZMQSend()
+                self.zmqreceive = ZMQReceive()
+                print(
+                    "Open GNURadio with ZMQ REQ source set at tcp://localhost:%d and ZMQ REP sink set at tcp://*:%d"
+                    % (self.zmqsend.port, self.zmqreceive.port)
+                )
+                print(
+                    "The data stream will be of the float type at 40MSPS (40MHz sample rate)"
+                )
+                print(
+                    "It will send the raw RF for further processing prior to demodulation (useful for RF EQ discovery "
+                    "and group delay compensation)"
+                )
+                print(
+                    "You might want to do this in single threaded decode mode (-t 1 parameter) - TODO: might not work correctly with --no_resample yet."
+                )
+            return
+
+        if "num_worker_threads" in kwargs:
+            kwargs = dict(kwargs)
+            kwargs["num_worker_threads"] = 0
+        elif len(base_args) >= 6:
+            base_args[5] = 0
+        else:
+            kwargs = dict(kwargs)
+            kwargs["num_worker_threads"] = 0
+
+        super(DemodCacheTape, self).__init__(*base_args, **kwargs)
+
+        self.q_out.put(None)
+        self.deqeue_thread.join()
+
+        self._process_context = multiprocessing.get_context()
+        self._worker_queues = [
+            self._process_context.Queue() for _ in range(requested_workers)
+        ]
+        process_output_queue = self._process_context.Queue()
+
+        self._job_dispatcher = _VHSJobDispatcher(self._worker_queues)
+        self.q_in = self._job_dispatcher
+        self.q_out = _VHSResultQueue(process_output_queue, self._job_dispatcher)
+
+        self.num_worker_threads = requested_workers
+        self.threads = []
+
+        rf_state = _make_worker_rf_state(self.rf)
+        for worker_queue in self._worker_queues:
+            process = self._process_context.Process(
+                target=_vhs_demod_process,
+                args=(
+                    worker_queue,
+                    process_output_queue,
+                    self.rf.__class__,
+                    rf_state,
+                ),
+                daemon=True,
             )
-            print(
-                "The data stream will be of the float type at 40MSPS (40MHz sample rate)"
-            )
-            print(
-                "It will send the raw RF for further processing prior to demodulation (useful for RF EQ discovery "
-                "and group delay compensation)"
-            )
-            print(
-                "You might want to do this in single threaded decode mode (-t 1 parameter) - TODO: might not work correctly with --no_resample yet."
-            )
+            process.start()
+            self.threads.append(process)
+
+        self.deqeue_thread = threading.Thread(target=self.dequeue, daemon=True)
+        self.deqeue_thread.start()
+
+    def end(self):
+        if not self.ended:
+            super(DemodCacheTape, self).end()
+            if hasattr(self, "_job_dispatcher"):
+                self._job_dispatcher.close()
 
     def worker(self, return_on_empty=False):
-        """Override to skip mtf stuff since that's laserdisc specific."""
         blocksrun = 0
         blockstime = 0
 
@@ -68,7 +323,7 @@ class DemodCacheTape(DemodCache):
                 blocksrun += 1
 
                 output["request"] = request
-                output["MTF"] = 0  # Not used so just set to 0 for time.
+                output["MTF"] = 0
 
                 self.q_out.put((blocknum, output))
             elif item[0] == "NEWPARAMS":
